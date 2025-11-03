4deb6a5d0c6a5bd007f731e455961821ae5e892e
wintun-devop <wintun.hlaing@outlook.com>
2025-11-03 21:43:16 +0630

    learn_rush

--- rust_exercise_01/src/main.rs
+++ rust_exercise_01/src/main.rs
@@ -2,7 +2,13 @@ fn main() {
     // variable and its outputs
     let name = "John";
     let age = 30;
-    println! ("{} is {} years old.", name, age);
+    println!("{} is {} years old.", name, age);
+
+    // variable redeclaration ok
     let x = 20;
-    println!("{}", x)
+    println!("x is {}.", x);
+    let x = 30;
+    println!("x is {}.", x);
+    let x = x + 1;
+    println!("x is {}.", x);
 }
