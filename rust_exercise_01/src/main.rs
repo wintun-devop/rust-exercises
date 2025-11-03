c37000d33f081e2ce3f929dbb977284634dc96ad
wintun-devop <wintun.hlaing@outlook.com>
2025-11-03 21:59:53 +0630

    basic concept of variables

--- rust_exercise_01/src/main.rs
+++ rust_exercise_01/src/main.rs
@@ -8,7 +8,17 @@ fn main() {
     let x = 20;
     println!("x is {}.", x);
     let x = 30;
+    {
+        /* own scope variable */
+        let x = 111;
+        println!("own scop x is {}.", x);
+    }
     println!("x is {}.", x);
     let x = x + 1;
     println!("x is {}.", x);
+    /* constants */
+    const PI:f64 = 3.14159265359;
+    println!("pi is {}.", PI);
+    const SECONDS_IN_ONE_MINUTES:u32 = 60;
+    println!("seconds in one minute is {}.", SECONDS_IN_ONE_MINUTES);
 }
